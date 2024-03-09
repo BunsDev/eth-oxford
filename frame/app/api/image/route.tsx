49922bc78033d73ae0e24c4ecf516c7924aedf2e
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 15:04:03 +0000

    method

--- frame/app/api/image/route.tsx
+++ frame/app/api/image/route.tsx
@@ -3,7 +3,7 @@ export const runtime = "edge";
 export const dynamic = "force-dynamic";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
     return new ImageResponse(
       (
         <div
