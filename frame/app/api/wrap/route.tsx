a94b2278c2ebef15a36495ea1718319afed5c466
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 14:58:37 +0000

    restore template

---
+++ frame/app/api/wrap/route.tsx
@@ -0,0 +1,32 @@
+import { headers } from "next/headers";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req: Request) {
+  const {
+    untrustedData: { inputText, fid },
+    trustedData: { messageBytes },
+  } = await req.json();
+  const headersList = headers();
+  const imageURL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/image?address=${inputText}`;
+  return new Response(
+    `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>ETH Wrapped</title>
+            <meta property="og:title" content="ETH Wrapped">
+            <meta property="og:image" content="${imageURL}">
+            <meta name="fc:frame" content="vNext">
+            <meta name="fc:frame:image" content="${imageURL}">
+           
+        </head>
+        <body>
+        </body>
+        </html>`,
+    {
+      status: 200,
+      headers: { "Content-Type": "text/html" },
+    }
+  );
+}
