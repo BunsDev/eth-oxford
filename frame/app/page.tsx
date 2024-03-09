5506b4b99b598141bc088b84f5ca8e2ba5a96be5
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 15:22:53 +0000

    tx

--- frame/app/page.tsx
+++ frame/app/page.tsx
@@ -11,7 +11,7 @@ const frameMetadata = getFrameMetadata({
     },
     {
       action: 'tx',
-      label: 'Send Base Sepolia',
+      label: 'Invest in this oportunity',
       target: `${NEXT_PUBLIC_URL}/api/tx`,
       postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     },
