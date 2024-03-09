de6ffecbad047e94a1a3faf23f4f1cff32b43ba4
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 13:05:23 +0000

    details

--- frame/app/api/traderDetail.ts
+++ frame/app/api/traderDetail.ts
@@ -1,29 +1,35 @@
-import { ImageResponse } from 'next/og'
+import { ImageResponse } from '@vercel/og';
 import { NextRequest } from "next/server";
 
+export const config = {
+  runtime: 'edge',
+};
+
 export async function GET(req: NextRequest) {
-    return new ImageResponse(
-        (
-          <div
-            style={{
-              fontSize: 40,
-              color: "black",
-              background: "white",
-              width: "100%",
-              height: "100%",
-              padding: "50px 200px",
-              textAlign: "center",
-              justifyContent: "center",
-              alignItems: "center",
-              display: "flex",
-            }}
-          >
-            Error fetching data :(. Please try again later.
-          </div>
-        ),
-        {
-          width: 1200,
-          height: 630,
-        }
-      );
+  return  new ImageResponse((
+    <div
+    style={{
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+      fontSize: 32,
+      fontWeight: 600,
+    }}
+  >
+    <svg
+      width="75"
+      viewBox="0 0 75 65"
+      fill="#000"
+      style={{ margin: '0 75px' }}
+    >
+      <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
+    </svg>
+    <div style={{ marginTop: 40 }}>Hello, World</div>
+  </div>
+  ))
+
 }
