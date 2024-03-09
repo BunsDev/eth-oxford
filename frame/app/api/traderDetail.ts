09d0c27504c3ae7294908bd54a766650313438cd
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 13:44:31 +0000

    trader image

--- frame/app/api/traderDetail.ts
+++ frame/app/api/traderDetail.ts
@@ -1,35 +1,24 @@
-import { ImageResponse } from '@vercel/og';
+// app/hello-today/opengraph-image.tsx
+import { ImageResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export const config = {
-  runtime: 'edge',
+export const size = {
+  width: 1200,
+  height: 630,
 };
 
-export async function GET(req: NextRequest) {
-  return  new ImageResponse((
-    <div
-    style={{
-      height: '100%',
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#fff',
-      fontSize: 32,
-      fontWeight: 600,
-    }}
-  >
-    <svg
-      width="75"
-      viewBox="0 0 75 65"
-      fill="#000"
-      style={{ margin: '0 75px' }}
-    >
-      <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
-    </svg>
-    <div style={{ marginTop: 40 }}>Hello, World</div>
-  </div>
-  ))
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
+export async function GET(req: NextRequest) {
+  const today = new Date();
+  const dayName = daysOfWeek[today.getDay()];
+  return new ImageResponse(<div>Happy {dayName}!</div>, { ...size });
 }
