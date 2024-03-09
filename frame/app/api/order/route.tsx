6016459550d7d430ef52c755f78bcc10c45a163b
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 16:16:02 +0000

    align

--- frame/app/api/order/route.tsx
+++ frame/app/api/order/route.tsx
@@ -7,7 +7,7 @@ export async function GET(req: NextRequest) {
   return new ImageResponse(
     (
       <div>
-        <h3>Investment Opportunity</h3>
+        <h5>Investment Opportunity</h5>
         <h5 style={{ textAlign: "center" }}>BTC/USD</h5>
         <h5 style={{ textAlign: "center" }}>Open price 70,000 USD</h5>
         <h5 style={{ textAlign: "center" }}>Close price 74,0000 USD</h5>
