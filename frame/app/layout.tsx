a126febc69b58b86dc3f85c572aba6b4916cb774
Ivan <ivan.alberquilla@gmail.com>
2024-03-09 14:30:51 +0000

    tx template

--- frame/app/layout.tsx
+++ frame/app/layout.tsx
@@ -1,21 +1,12 @@
-import type { Metadata } from "next";
-import { Figtree } from "next/font/google";
-import "./globals.css";
-import React from "react";
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+};
 
-const figtree = Figtree({
-  subsets: ["latin"],
-  variable: "--font-figtree",
-});
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${figtree.variable}`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- frame/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
-        GRAPH_API_KEY: process.env.GRAPH_API_KEY,
-    },
-};
-
-export default nextConfig;
